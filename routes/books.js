f26c1f84506eebc2c3926084439de030c0bfd5c1
marijagaliatovic <92432581+marijagaliatovic@users.noreply.github.com>
2024-03-01 11:41:39 +0100

    Finished book routes

--- routes/books.js
+++ routes/books.js
@@ -30,7 +30,7 @@ router.get('/', async (req, res) => {
 
 //New Book Route
 router.get('/new', async (req, res)=>{
-   renderNewpage(res, new Book())
+   renderNewPage(res, new Book())
 })
 
 //create book route
@@ -45,17 +45,95 @@ router.post('/', async(req, res)=>{
     saveCover(book, req.body.cover)
     try{
         const newBook = await book.save() //book is saved to db
-        //res.redirect(`books/${newBook.id}`)
-        res.redirect('books') //we are redirected to books page
-    }catch(error){
-        console.log(error)
-        renderNewpage(res, book, true)
+        res.redirect(`books/${newBook.id}`)
+    }catch{
+        renderNewPage(res, book, true)
     }
 })
 
+//Show book route
+router.get('/:id', async(req,res) => {
+    try{
+        const book = await Book.findById(req.params.id)
+                                //.populate('author') 
+                               // .exec() //we have to use populate function to "populate" selected value with all information, in our example in book collection author has only id, and with this we are able to get the name also (fetches all author info before fetching the book)
+        //in case I deleted the author before puting constrains, book has author but it's not in the base, in that case don't throw error but return
+        if(book.author == null){
+            return
+        }
+        res.render('books/show', {book:book})
+       
+    }catch{
+        res.redirect('/')
+    }
+})
 
+//Edit Book Route
+router.get('/:id/edit', async (req, res) => {
+    try {
+    const book = await Book.findById(req.params.id)
+      renderEditPage(res, book)
+    } catch(err) {
+        console.log(err)
+      res.redirect('/')
+    }
+  })
 
-async function renderNewpage(res, book, hasError = false){
+ //Update book route
+router.put('/:id', async(req, res)=>{
+    let book 
+    try{
+        book = await Book.findById(req.params.id)
+        book.title = req.body.title
+        book.author= req.body.author
+        book.publishDate = new Date(req.body.publishDate)
+        book.pageCount = req.body.pageCount
+        book.description = req.body.description
+        if(req.body.cover != null && req.body.cover !== ''){
+            saveCover(book, req.body.cover) // we don't want to delite the book cover if user doesn't change it
+        }
+        await book.save()
+        res.redirect(`/books/${book.id}`)
+    }catch(error){
+        console.log(error)
+        if(book != null){
+            renderNewPage(res, book, true)
+        }
+        else{
+            redirect('/')
+        }
+    }
+})
+
+//Delete Book Page
+router.delete('/:id', async (req,res) => {
+    let book
+    try{
+        book = await Book.findById(req.params.id)
+        await book.deleteOne()
+        res.redirect('/books')
+    }catch(err){
+        console.log(err)
+        if(book !=null ){
+            res.render('books/show', {
+                book:book,
+                errorMessage: 'Could not remove book'
+            })
+        }else{
+            res.redirect('/')
+        }
+    }
+})
+
+async function renderNewPage(res, book, hasError = false){
+    renderFormPage(res, book, 'new', hasError)
+}
+
+async function renderEditPage(res, book, hasError = false){
+    renderFormPage(res, book, 'edit', hasError)
+}
+
+async function renderFormPage(res, book, form, hasError = false){
     try{
         const authors = await Author.find({}) //fetches all authors
         const params = {
@@ -63,13 +141,21 @@ async function renderNewpage(res, book, hasError = false){
             book:book
         }
         if(hasError){
-            params.errorMessage = 'Error creating book!'
+            if(form === "edit")
+            {
+                params.errorMessage = 'Error editing book!'
+            }
+            else{
+                params.errorMessage = 'Error creating book!'
+            }
         }
-        res.render('books/new', params) 
-    }catch{
+        res.render(`books/${form}`, params)
+    }catch(err){
+        console.log(err)
         res.redirect('/books')
     }
 }
+
 
 //the imported image is encoded and we have to transfrom it from JSON format to JS object
 function saveCover(book, coverEncoded) {
